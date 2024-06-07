bb763442c87b4096f4a72f614fdd977196337e74
MarkSerna <marcoesernal@gmail.com>
2024-06-07 18:14:56 -0500

    Se creó el proyecto Festivos Colombia

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='calendario_colombiano',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    description='Biblioteca para manejar el calendario colombiano con festivos y fechas especiales',
+    author='Marco Eduar Serna Lopez',
+    author_email='marcoesernal@gmail.com',
+    url='https://github.com/tu_usuario/calendario_colombiano',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
